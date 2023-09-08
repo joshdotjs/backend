b25ce1526e742b412dfb68ef05a1ab2e8cd381a2
joshdotjs <joshdotjs@gmail.com>
2023-09-08 17:57:03 -0500

    removed sigs rollback

--- src/db/migrations/20230831082544_init_migrations.js
+++ src/db/migrations/20230831082544_init_migrations.js
@@ -17,6 +17,5 @@ exports.up = async (knex) => {
 // ==============================================
 
 exports.down = async (knex) => {
-  await knex.schema.dropTableIfExists('sigs');
   await knex.schema.dropTableIfExists('users');
 };
