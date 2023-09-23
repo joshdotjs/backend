19dbe5a9c2e37eaf904e05ce75ededf4408dfd0d
joshdotjs <joshdotjs@gmail.com>
2023-09-23 09:26:55 -0500

    deploy

--- backend/cypress/e2e/UI.cy.js
+++ backend/cypress/e2e/UI.cy.js
@@ -13,47 +13,31 @@ describe('UI :: hidden text', () => {
 
 
   beforeEach(() => {
-    cy.visit('http://localhost:9000/');
+    // cy.visit('http://localhost:9000/'); // backend
+    cy.visit('http://localhost:5173/'); // frontend
     cy.task('resetDB').then(cy.log);
   })
 
   // -----------------------------------------------------
 
-  it('should NOT display text when button is NOT clicked', () => {
-    cy.visit('http://localhost:9000/');
-
-    // cy.task("connectDB").then(cy.log)
-    // cy.task("connectDB","SELECT NOW()").then(cy.log);
-    // cy.task("connectDB","SELECT NOW()").then((response) => {
-    //   cy.log(response);
-    // });
-
-    cy.task('connectDB', 'SELECT * FROM users').then((rows) => {
-      rows.forEach(row => cy.log(row.email));
-    });
-
-  });
-
-  // -----------------------------------------------------
-});
-
-// ========================================================
-
-describe('UI :: type into field', () => {
-
-  // -----------------------------------------------------
-
-  // it('should display the same text to the output that was typed into the input', () => {
-  //   cy.visit('http://localhost:9000/');
-
-  //   const text = 'hello world';
-
-  //   cy.get('#input-field').type(text);
-  //   cy.get('#output-field').should('contain.text', text);
+  // it('access DB withing tests', () => {
+  //   // cy.task("connectDB").then(cy.log)
+  //   // cy.task("connectDB","SELECT NOW()").then(cy.log);
+  //   // cy.task("connectDB","SELECT NOW()").then((response) => {
+  //   //   cy.log(response);
+  //   // });
+  //   cy.task('connectDB', 'SELECT * FROM users').then((rows) => {
+  //     rows.forEach(row => cy.log(row.email));
+  //   });
   // });
 
   // -----------------------------------------------------
 
+  it('should add item to cart', () => {
+    
+  });
+
+  // -----------------------------------------------------
 });
 
 // ========================================================
