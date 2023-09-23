f18854543b9a51fc6e13d9d439d4b7480d55659d
joshdotjs <joshdotjs@gmail.com>
2023-09-22 20:51:27 -0500

    refresh DB before each e2e test - use the testing DB

--- cypress.config.js
+++ cypress.config.js
@@ -2,59 +2,22 @@ const { defineConfig } = require("cypress");
 const { Client } = require('pg');
 const path = require('path');
 
-console.log('JOSH');
-console.log('JOSH');
-console.log('JOSH');
-console.log('JOSH');
-console.log('JOSH');
-console.log('JOSH');
-console.log('JOSH');
-console.log('JOSH');
-console.log('JOSH');
-console.log('JOSH');
-console.log('JOSH');
-console.log('JOSH');
-console.log('JOSH');
-console.log('JOSH');
-console.log('JOSH');
-console.log('JOSH');
-console.log('JOSH');
-console.log('JOSH');
-console.log('JOSH');
-console.log('JOSH');
-console.log('JOSH');
-console.log('JOSH');
-console.log('JOSH');
-console.log('JOSH');
-console.log('JOSH');
-console.log('JOSH');
-console.log('JOSH');
-console.log('JOSH');
-console.log('JOSH');
-console.log('JOSH');
-console.log('JOSH');
-console.log('JOSH');
-console.log('JOSH');
-console.log('JOSH');
 require("dotenv").config();
-console.log(process.env.DB_USER);
-console.log(__filename);
 require(path.join(__dirname, 'src', 'util', 'path.js'));
 required('util/console');
-
-console.magenta('josh');
 const db = required('db/db');
 
-
-// step 1: get .env working
-// step 2: do the db.js code in here 
-
+// ==============================================
+// ==============================================
+// ==============================================
+// ==============================================
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
       on("task", {
+        // ======================================
         async connectDB(query){
           const client = new Client({
             user: process.env.DB_USER,
@@ -70,13 +33,22 @@ module.exports = defineConfig({
           await client.end();
           return res.rows;
         },
-        async josh() {
-          return 'INSIDE JOSH';
-        },
-        // async rollback() {
-        //   await db.migrate.rollback();
-        //   return 'rollback()';
-        // }
+        // ======================================
+        async resetDB() {
+          console.yellow('rolling back...');
+          await db.migrate.rollback();
+
+          console.magenta('migrating...');
+          await db.migrate.latest();
+
+          console.blue('seeding...');
+          await db.seed.run();
+
+          console.green('DB has been reset! 😊');
+
+          return 'resetDB() complete';
+        }
+        // ======================================
       });
     },
   },
--- cypress/e2e/UI.cy.js
+++ cypress/e2e/UI.cy.js
@@ -5,56 +5,34 @@
 // require('../../src/util/path');
 // const db = require('../../src/db/db');
 
-
-
 // ========================================================
 
 describe('UI :: hidden text', () => {
 
   // -----------------------------------------------------
 
+
+  beforeEach(() => {
+    cy.visit('http://localhost:9000/');
+    cy.task('resetDB').then(cy.log);
+  })
+
+  // -----------------------------------------------------
+
   it('should NOT display text when button is NOT clicked', () => {
     cy.visit('http://localhost:9000/');
-    // cy.get('#display-text-button').click(); // clicks center of element
-    // cy.get('#display-text').should('have.class', 'hidden');
-    // cy.get('#display-text').should('not.be.visible');
 
     // cy.task("connectDB").then(cy.log)
-    cy.task("connectDB","SELECT NOW()").then(cy.log);
-    cy.task("connectDB","SELECT NOW()").then((response) => {
-      cy.log(response);
-    });
-
-    cy.task('connectDB', 'SELECT * FROM users').then((response) => {
-      cy.log(response[0].email);
-    });
+    // cy.task("connectDB","SELECT NOW()").then(cy.log);
+    // cy.task("connectDB","SELECT NOW()").then((response) => {
+    //   cy.log(response);
+    // });
 
     cy.task('connectDB', 'SELECT * FROM users').then((rows) => {
-      cy.log(rows[0].email);
+      rows.forEach(row => cy.log(row.email));
     });
 
-
-    cy.task('josh').then(cy.log);
   });
-
-  // -----------------------------------------------------
-
-  // it('should display text when button is clicked', () => {
-  //   cy.visit('http://localhost:9000/');
-  //   cy.get('#display-text-button').click(); // clicks center of element
-  //   cy.get('#display-text').should('not.have.class', 'hidden');
-  //   cy.get('#display-text').should('be.visible');
-  // });
-
-  // -----------------------------------------------------
-
-  // it('should NOT display text when button is clicked twice', () => {
-  //   cy.visit('http://localhost:9000/');
-  //   cy.get('#display-text-button').click(); // clicks center of element
-  //   cy.get('#display-text-button').click(); // clicks center of element
-  //   cy.get('#display-text').should('have.class', 'hidden');
-  //   cy.get('#display-text').should('not.be.visible');
-  // });
 
   // -----------------------------------------------------
 });
