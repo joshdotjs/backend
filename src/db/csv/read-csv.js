d2a454f191bed4ea50e49c479aa2748e4335a86e
joshdotjs <joshdotjs@gmail.com>
2023-09-30 16:43:17 -0500

    read products from CSV during seeding process

--- src/db/csv/read-csv.js
+++ src/db/csv/read-csv.js
@@ -1,15 +1,19 @@
 const csv = require('csv-parser')
 const fs = require('fs')
-const results = [];
 
-fs.createReadStream('./products.csv')
-  // .pipe(csv({ separator: '\t' }))
-  .pipe(csv())
-  .on('data', (data) => results.push(data))
-  .on('end', () => {
-    console.log(results);
-    // [
-    //   { NAME: 'Daffy Duck', AGE: '24' },
-    //   { NAME: 'Bugs Bunny', AGE: '22' }
-    // ]
-  });
+
+const readCSV = () => {
+  const results = [];
+  
+  fs.createReadStream('./products.csv')
+    // .pipe(csv({ separator: '\t' }))
+    .pipe(csv())
+    .on('data', (data) => results.push(data))
+    // .on('end', () => {
+    //   console.log(results);
+    // });
+
+  return results;
+};
+
+module.exports = { readCSV };
