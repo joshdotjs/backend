039eacb4194e44429fc72f743f1125b17d29fa86
joshdotjs <joshdotjs@gmail.com>
2023-10-13 16:06:39 -0500

    update

--- src/util/string.js
+++ src/util/string.js
@@ -1,31 +1,36 @@
-"use strict";
 function truncateString({ str, len = 6 }) {
-    if (str.length > len) {
-        return str.substring(0, len) + "...";
-    }
-    else {
-        return str;
-    }
+  if (str.length > len) {
+    return str.substring(0, len) + "...";
+  }
+  else {
+    return str;
+  }
 }
+
 // ==============================================
+
 function truncateStringFront({ str, len = 6 }) {
-    if (str.length > len) {
-        return "..." + str.substring(str.length - len, str.length);
-    }
-    else {
-        return str;
-    }
+  if (str.length > len) {
+    return "..." + str.substring(str.length - len, str.length);
+  }
+  else {
+    return str;
+  }
 }
+
 // ==============================================
+
 function zeroPad(n, width = 2) {
-    // padZero(7)    =>   "07"
-    // padZero(7, 2) =>   "07"
-    // padZero(7, 3) =>  "007
-    return String(n).padStart(width, '0');
+  // padZero(7)    =>   "07"
+  // padZero(7, 2) =>   "07"
+  // padZero(7, 3) =>  "007
+  return String(n).padStart(width, '0');
 }
+
 // ==============================================
+
 module.exports = {
-    truncateString,
-    truncateStringFront,
-    zeroPad
+  truncateString,
+  truncateStringFront,
+  zeroPad
 };
