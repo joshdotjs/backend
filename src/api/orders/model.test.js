85f581ea1c1d15f1c6eeec8fafeaa22a3d47057c
joshdotjs <joshdotjs@gmail.com>
2023-10-19 05:00:49 -0500

    unit test OrdersModel.create( order )

--- src/api/orders/model.test.js
+++ src/api/orders/model.test.js
@@ -57,4 +57,19 @@ describe('Model - Orders', () => {
 
   // ============================================
 
+  it('create( order )', async () => {
+    const created_order = await OrdersModel.create({ uuid: 'abc123', user_id: 1, total: 100, status: 1 });
+    console.log('created_order: ', created_order);
+    expect(created_order[0].id).toBe(4);
+    expect(created_order[0].uuid).toBe('abc123');
+    expect(created_order[0].user_id).toBe(1);
+    expect(created_order[0].total).toBe(100);
+    expect(created_order[0].status).toBe(1);
+    // NOTE: created_at and updated_at are not returned by this model function
+    // expect(created_order[0].created_at).toBeDefined();
+    // expect(created_order[0].updated_at).toBeDefined();
+  });
+
+  // ============================================
+
 });
