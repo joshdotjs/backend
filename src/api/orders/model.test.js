09fd26a8f07a43c79e7a44b3eeaa72e7e5897724
joshdotjs <joshdotjs@gmail.com>
2023-10-19 05:08:00 -0500

    unit test OrdersModel.getByUuid( uuid )

--- src/api/orders/model.test.js
+++ src/api/orders/model.test.js
@@ -45,7 +45,7 @@ describe('Model - Orders', () => {
 
   it('getAll()', async () => {
     const orders = await OrdersModel.getAll();
-    console.log('orders: ', orders);
+    // console.log('orders: ', orders);
     expect(typeof orders[0].id).toBe('number');
     expect(typeof orders[0].uuid).toBe('string');
     expect(typeof orders[0].status).toBe('number');
@@ -59,7 +59,7 @@ describe('Model - Orders', () => {
 
   it('create( order )', async () => {
     const created_order = await OrdersModel.create({ uuid: 'abc123', user_id: 1, total: 100, status: 1 });
-    console.log('created_order: ', created_order);
+    // console.log('created_order: ', created_order);
     expect(created_order[0].id).toBe(4);
     expect(created_order[0].uuid).toBe('abc123');
     expect(created_order[0].user_id).toBe(1);
@@ -68,6 +68,50 @@ describe('Model - Orders', () => {
     // NOTE: created_at and updated_at are not returned by this model function
     // expect(created_order[0].created_at).toBeDefined();
     // expect(created_order[0].updated_at).toBeDefined();
+  });
+
+  // ============================================
+
+  it('getByUuid( uuid )', async () => {
+    const created_order = await OrdersModel.create({ uuid: 'abc123', user_id: 1, total: 100, status: 1 });
+    // console.log('created_order: ', created_order);
+    expect(created_order[0].uuid).toBe('abc123');
+
+    const array = await OrdersModel.getByUuid('abc123');
+    expect(array.length).toBe(1);
+    expect(array[0].id).toBe(4);
+    expect(array[0].uuid).toBe('abc123');
+    expect(array[0].user_id).toBe(1);
+    expect(array[0].total).toBe(100);
+    expect(array[0].status).toBe(1);
+  });
+
+  // ============================================
+
+  // TODO: 
+  it('createOrder2Product', async () => {
+    expect(1).toBe(1);
+  });
+
+  // ============================================
+
+  // TODO: 
+  it('getProductsInOrderById', async () => {
+    expect(1).toBe(1);
+  });
+
+  // ============================================
+
+  // TODO: 
+  it('updateStatus', async () => {
+    expect(1).toBe(1);
+  });
+
+  // ============================================
+
+  // TODO: 
+  it('getFiltered', async () => {
+    expect(1).toBe(1);
   });
 
   // ============================================
