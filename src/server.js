47542f1e8fb968dfed71c9b6352bddb87abccee0
joshdotjs <joshdotjs@gmail.com>
2023-10-20 13:49:59 -0500

    remove email

--- src/server.js
+++ src/server.js
@@ -81,23 +81,23 @@ server.use('*', (req, res, next) => {
 // ==============================================
 
 
-const sgMail = require('@sendgrid/mail')
-sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-const msg = {
-  to: 'jhollow6@asu.edu', // Change to your recipient
-  from: 'joshDotJS@gmail.com', // Change to your verified sender
-  subject: 'Sending with SendGrid is Fun',
-  text: 'and easy to do anywhere, even with Node.js',
-  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-}
-sgMail
-  .send(msg)
-  .then(() => {
-    console.log('Email sent')
-  })
-  .catch((error) => {
-    console.error(error)
-  })
+// const sgMail = require('@sendgrid/mail')
+// sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+// const msg = {
+//   to: 'jhollow6@asu.edu', // Change to your recipient
+//   from: 'joshDotJS@gmail.com', // Change to your verified sender
+//   subject: 'FROM LOCAL SERVER',
+//   text: 'This is from the lcoal server',
+//   html: '<strong>this is html bitch!</strong>',
+// }
+// sgMail
+//   .send(msg)
+//   .then(() => {
+//     console.log('Email sent')
+//   })
+//   .catch((error) => {
+//     console.error(error)
+//   })
 
 // ==============================================
 
