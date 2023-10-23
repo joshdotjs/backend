988da91d2ab5abb4cf831f613fd466be9df281b0
joshdotjs <joshdotjs@gmail.com>
2023-10-23 04:24:29 -0500

    e2e: change quantity of items in cart and remove items from cart

--- cypress/e2e/index.cy copy.js
+++ cypress/e2e/index.cy copy.js
@@ -574,11 +574,52 @@ describe('Cart / Checkout', () => {
 
   // -----------------------------------------------------
 
-  // TEST: Removing items from cart works correctly
+  // TEST: Changing quantity of items in cart and removing items from cart works correctly
+  it('change quantity of items in cart and remove items from cart', () => {
+  
+    // add item to cart
+    get('product-card-3-add-button').click(); // click add to cart button
+    get('cart-item-3-quantity').contains('1'); // test quantity
+    get('cart-total').contains('$3.00'); // test cart total is correct
+    get('cart-item-3-quantity-trash').should('exist'); // trash icon SHOULD display
+    get('cart-item-3-quantity-minus').should('not.exist'); // minus icon should NOT display
+    
+    // increase quantity
+    get('cart-item-3-quantity-plus').click(); // click plus button
+    get('cart-total').contains('$6.00'); // test cart total is correct
+    get('cart-item-3-quantity').contains('2'); // test quantity
+    get('cart-item-3-quantity-trash').should('not.exist'); // trash icon NOT should display
+    get('cart-item-3-quantity-minus').should('exist'); // minus icon SHOULD  display
 
-  // -----------------------------------------------------
+    // increase quantity
+    get('cart-item-3-quantity-plus').click(); // click plus button
+    get('cart-total').contains('$9.00'); // test cart total is correct
+    get('cart-item-3-quantity').contains('3'); // test quantity
+    get('cart-item-3-quantity-trash').should('not.exist'); // trash icon NOT should display
+    get('cart-item-3-quantity-minus').should('exist'); // minus icon SHOULD  display
 
-  // TEST: Changing quantity of items in cart works correctly
+    // decrease quantity
+    get('cart-item-3-quantity-minus').click(); // click plus button
+    get('cart-total').contains('$6.00'); // test cart total is correct
+    get('cart-item-3-quantity').contains('2'); // test quantity
+    get('cart-item-3-quantity-trash').should('not.exist'); // trash icon NOT should display
+    get('cart-item-3-quantity-minus').should('exist'); // minus icon SHOULD  display
+
+    // decrease quantity
+    get('cart-item-3-quantity-minus').click(); // click plus button
+    get('cart-total').contains('$3.00'); // test cart total is correct
+    get('cart-item-3-quantity').contains('1'); // test quantity
+    get('cart-item-3-quantity-trash').should('exist'); // trash icon SHOULD display
+    get('cart-item-3-quantity-minus').should('not.exist'); // minus icon should NOT display
+
+    // remove item from cart
+    get('cart-item-3-quantity-trash').click(); // click trash can button
+    get('cart-total').contains('$0.00'); // test cart total is correct
+    get('cart-item-3').should('not.exist'); // test item is removed from cart
+    
+    // close cart with the (X) close button - NOTE Cart is closed implicitly when all items are removed
+    // get('cart-drawer-close-button').click();
+  });
 
   // -----------------------------------------------------
 
