c7f61deffbd2f316fa6729428b5a781fe0cc2b06
joshdotjs <joshdotjs@gmail.com>
2023-10-26 06:18:09 -0500

    all e2e tests

--- cypress/e2e/index----.cy.js
+++ cypress/e2e/index----.cy.js
@@ -587,6 +587,100 @@ describe('Admin Orders', () => {
 
   // -----------------------------------------------------
 
+  // TEST: filtering (change time)
+  it('test filtering [status]', () => {
+
+    // RESET DB - undo the altering of the dates of the orders in the previous tests 
+    cy.task('resetDB').then(cy.log);
+
+    // log user in
+    get('navlink-Login-desktop').click();
+    get('auth-email-text-field').type('josh@josh.com');
+    get('auth-password-text-field').type('josh');
+    get('auth-login-button').click();
+
+    // unsellect all status options
+    get('admin-orders-status-dropdown').click(); // open status dropdown
+    get('status-dropdown-option-Pending').click();
+    get('status-dropdown-option-Preparing').click();
+    get('status-dropdown-option-Ready').click();
+    get('status-dropdown-option-Done').click();
+    cy.get('body').click(); // close status dropdown
+    cy.wait(500);
+
+    // cy.wait(500);
+    get('admin-order-1').should('not.exist');
+    get('admin-order-2').should('not.exist');
+    get('admin-order-3').should('not.exist');
+
+    // select only status: Pending
+    get('admin-orders-status-dropdown').click(); // open status dropdown
+    get('status-dropdown-option-Pending').click();
+    get('admin-order-1').should('exist');
+    get('admin-order-2').should('exist');
+    get('admin-order-3').should('exist');
+    cy.get('body').click(); // close status dropdown
+
+    // select all statuses
+    get('admin-orders-status-dropdown').click(); // open status dropdown
+    get('status-dropdown-option-Preparing').click();
+    get('status-dropdown-option-Ready').click();
+    get('status-dropdown-option-Done').click();
+    get('admin-order-1').should('exist');
+    get('admin-order-2').should('exist');
+    get('admin-order-3').should('exist');
+    cy.get('body').click(); // close status dropdown
+
+    // change status of each order
+    get('admin-order-1').click(); // open the order details accordion
+    get('admin-order-1--status-button--preparing').click(); // change order status
+    get('admin-order-2').click(); // open the order details accordion
+    get('admin-order-2--status-button--ready').click(); // change order status
+    get('admin-order-3').click(); // open the order details accordion
+    get('admin-order-3--status-button--done').click(); // change order status
+
+    // select only the preparing status
+    get('admin-orders-status-dropdown').click(); // open status dropdown
+    get('status-dropdown-option-Pending').click();
+    get('status-dropdown-option-Ready').click();
+    get('status-dropdown-option-Done').click();
+    get('admin-order-1').should('exist');
+    get('admin-order-2').should('not.exist');
+    get('admin-order-3').should('not.exist');
+    cy.get('body').click(); // close status dropdown
+
+    // select only the ready status
+    get('admin-orders-status-dropdown').click(); // open status dropdown
+    get('status-dropdown-option-Preparing').click();
+    get('status-dropdown-option-Ready').click();
+    get('admin-order-1').should('not.exist');
+    get('admin-order-2').should('exist');
+    get('admin-order-3').should('not.exist');
+    cy.get('body').click(); // close status dropdown
+
+    // select only the done status
+    get('admin-orders-status-dropdown').click(); // open status dropdown
+    get('status-dropdown-option-Ready').click();
+    get('status-dropdown-option-Done').click();
+    get('admin-order-1').should('not.exist');
+    get('admin-order-2').should('not.exist');
+    get('admin-order-3').should('exist');
+    cy.get('body').click(); // close status dropdown
+
+    // select both preparing and ready statuses
+    get('admin-orders-status-dropdown').click(); // open status dropdown
+    get('status-dropdown-option-Done').click();
+    get('status-dropdown-option-Preparing').click();
+    get('status-dropdown-option-Ready').click();
+    get('admin-order-1').should('exist');
+    get('admin-order-2').should('exist');
+    get('admin-order-3').should('not.exist');
+    cy.get('body').click(); // close status dropdown
+    
+  }); // it()  
+
+  // -----------------------------------------------------
+
   // TEST: real time updates
   //  -create a way to change the update time in the UI
   //  -change the update time to a short time
