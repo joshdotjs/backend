0938187dc1817db86da25d12efd945bdb5c188df
joshdotjs <joshdotjs@gmail.com>
2023-11-03 08:25:53 -0500

    web socket

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 require('./src/util/path'); // required() and rootPath()
-const server = required('server');
+const app = required('server');
 
-server.listen(process.env.PORT, () => {
-  const add = require('./ts');
+// ==============================================
+
+const server = app.listen(process.env.PORT, () => {
+  // const add = require('./ts');
   // console.log('add(1, 2): ', add(1, 2));
-  console.cyan(`server.js: http://localhost:${process.env.PORT}`);
+  const str = `server.js: http://localhost:${process.env.PORT}`;
+  console.cyan(str);
+});
+
+// ==============================================
+
+// web sockets:
+const { Server } = require('socket.io');
+const io = new Server(server);
+io.on('connection', (socket) => {
+  console.log('a user connected');
 });
