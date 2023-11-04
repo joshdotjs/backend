2201f2261c2b31a64a3be16130e9d07c2aba805f
joshdotjs <joshdotjs@gmail.com>
2023-11-04 09:08:11 -0500

    trash

--- index.js
+++ index.js
@@ -27,14 +27,28 @@ const io = new Server(server);
 //   });
 // });
 
+// io.on('connection', (socket) => {
+//   console.log('a user connected');
+  
+//   socket.on('disconnect', () => {
+//     console.log('user disconnected');
+//   });
+
+//   socket.on('chat message', (msg) => {
+//     console.log('message: ' + msg);
+//   });
+// });
+
 io.on('connection', (socket) => {
   console.log('a user connected');
-  
+
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
 
   socket.on('chat message', (msg) => {
     console.log('message: ' + msg);
+
+    io.emit('chat message', msg);
   });
 });
--- public/js/home-page.js
+++ public/js/home-page.js
@@ -234,6 +234,7 @@ const socket = io();
 
 const form = document.getElementById('socket-form');
 const input = document.getElementById('input-socket');
+const messages = document.getElementById('messages');
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
@@ -241,4 +242,11 @@ form.addEventListener('submit', (e) => {
     socket.emit('chat message', input.value);
     input.value = '';
   }
+});
+
+socket.on('chat message', (msg) => {
+  const item = document.createElement('li');
+  item.textContent = msg;
+  messages.appendChild(item);
+  window.scrollTo(0, document.body.scrollHeight);
 });
