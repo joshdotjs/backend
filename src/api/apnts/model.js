c35ec2fa34fb37fd922909c2ea4e8dca58fedd41
joshdotjs <joshdotjs@gmail.com>
2023-11-20 19:09:14 -0600

    on /api/apnt endpoint joining tables

--- src/api/apnts/model.js
+++ src/api/apnts/model.js
@@ -3,9 +3,24 @@ const db = required('db/db');
 // ==============================================
 
 exports.getAll = () => {
-  return db('apnts')
-    .select('user_id', 'apnt_type_id', 'date_time')
-    .orderBy('id');
+  // return db('apnts')
+  //   .select('user_id', 'apnt_type_id', 'date_time')
+  //   .orderBy('id');
+
+    return db('apnt_types')
+    .join('apnts', 'apnt_types.id', 'apnts.apnt_type_id')
+    .join('users', 'users.id', 'apnts.user_id')
+    .select(
+      'apnt_types.name as type',
+      'apnts.date_time as date_time',
+      'apnt_types.price as price',
+      'apnts.user_id as user_id',
+      'users.first_name as first_name',
+      'users.last_name as last_name',
+      'users.email as email',
+    );
+    // .where('o2p.order_id', order_id);
+
 }
 
 // ==============================================
